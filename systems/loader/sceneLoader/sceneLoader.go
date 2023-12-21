437b78c575fec7da63c7f69adb9ae590d0671b72
kainn9 <kaindoneson@gmail.com>
2023-12-21 15:03:35 -0500

    shrinking window

--- systems/loader/sceneLoader/sceneLoader.go
+++ systems/loader/sceneLoader/sceneLoader.go
@@ -112,44 +112,25 @@ func (sys SceneLoaderSystem) create(collisionMatrix [][]int, gridSize int) {
 	for i := 0; i < len(collisionMatrix); i++ {
 		for j := 0; j < len(collisionMatrix[i]); j++ {
 			if collisionMatrix[i][j] == 1 {
+
 				// Find the maximum rectangle size for the current block
 				maxX, maxY := findMaxRectangle(collisionMatrix, i, j, gridSize)
 
-				// Check if there are obstacles within the rectangle
-				if !hasObstacles(collisionMatrix, i, j, maxX, maxY) {
-					// Create vertices for the merged rectangle
-					vert1 := tBokiVec.Vec2{X: float64(j * gridSize), Y: float64(i * gridSize)}
-					vert2 := tBokiVec.Vec2{X: float64((j+maxX)*gridSize - 1), Y: float64(i * gridSize)}
-					vert3 := tBokiVec.Vec2{X: float64((j+maxX)*gridSize - 1), Y: float64((i+maxY)*gridSize - 1)}
-					vert4 := tBokiVec.Vec2{X: float64(j * gridSize), Y: float64((i+maxY)*gridSize - 1)}
+				// Create vertices for the merged rectangle
+				vert1 := tBokiVec.Vec2{X: float64(j * gridSize), Y: float64(i * gridSize)}
+				vert2 := tBokiVec.Vec2{X: float64((j+maxX)*gridSize - 1), Y: float64(i * gridSize)}
+				vert3 := tBokiVec.Vec2{X: float64((j+maxX)*gridSize - 1), Y: float64((i+maxY)*gridSize - 1)}
+				vert4 := tBokiVec.Vec2{X: float64(j * gridSize), Y: float64((i+maxY)*gridSize - 1)}
 
-					// Create a slice of vertices for the merged rectangle
-					vertices := []tBokiVec.Vec2{vert1, vert2, vert3, vert4}
+				// Create a slice of vertices for the merged rectangle
+				vertices := []tBokiVec.Vec2{vert1, vert2, vert3, vert4}
 
-					// Add rigid body to the scene using scenesUtil.AddBlockEntityPoly
-					scenesUtil.AddBlockEntityPoly(sys.scene, vertices)
+				// Add rigid body to the scene using scenesUtil.AddBlockEntityPoly
+				scenesUtil.AddBlockEntityPoly(sys.scene, vertices)
 
-					// Mark the merged blocks as processed
-					markAsProcessed(collisionMatrix, i, j, maxX, maxY)
-				} else {
-					// If the condition is not met, check if a smaller rectangle is necessary
-					if maxX > 1 || maxY > 1 {
-						// Create a smaller rectangle to handle the tile without dropping it
-						vert1 := tBokiVec.Vec2{X: float64(j * gridSize), Y: float64(i * gridSize)}
-						vert2 := tBokiVec.Vec2{X: float64((j+1)*gridSize - 1), Y: float64(i * gridSize)}
-						vert3 := tBokiVec.Vec2{X: float64((j+1)*gridSize - 1), Y: float64((i+1)*gridSize - 1)}
-						vert4 := tBokiVec.Vec2{X: float64(j * gridSize), Y: float64((i+1)*gridSize - 1)}
+				// Mark the merged blocks as processed
+				markAsProcessed(collisionMatrix, i, j, maxX, maxY)
 
-						// Create a slice of vertices for the smaller rectangle
-						vertices := []tBokiVec.Vec2{vert1, vert2, vert3, vert4}
-
-						// Add rigid body to the scene using scenesUtil.AddBlockEntityPoly
-						scenesUtil.AddBlockEntityPoly(sys.scene, vertices)
-
-						// Mark the individual block as processed (not the entire rectangle)
-						markAsProcessed(collisionMatrix, i, j, 1, 1)
-					}
-				}
 			}
 		}
 	}
@@ -182,7 +163,35 @@ func findMaxRectangle(collisionMatrix [][]int, row, col, gridSize int) (maxX, ma
 		maxY++
 	}
 
-	return maxX, maxY
+	// Check if there are obstacles within the rectangle, if so, reduce the rectangle size vertically.
+	currX, currY := maxX, maxY
+
+	for hasObstacles(collisionMatrix, row, col, currX, currY) && currY > 1 {
+		currY--
+	}
+
+	// If vertical reduction is successful, return the new rectangle size.
+	if currY > 1 {
+		return currX, currY
+	}
+
+	// Otherwise, reduce the rectangle size horizontally.
+
+	currX, currY = maxX, maxY
+
+	for hasObstacles(collisionMatrix, row, col, currX, currY) && currX > 1 {
+		currX--
+	}
+
+	// If horizontal reduction is successful, return the new rectangle size.
+	if currX > 1 {
+		return currX, currY
+	}
+
+	// Otherwise, fatal error.
+	log.Fatalf("Error finding max rectangle size for block at (%d, %d)", row, col)
+
+	return 1, 1
 }
 
 // Mark the merged blocks as processed
@@ -195,3 +204,20 @@ func markAsProcessed(collisionMatrix [][]int, row, col, maxX, maxY int) {
 		}
 	}
 }
+
+// if maxX > 1 || maxY > 1 {
+// 	// Create a smaller rectangle to handle the tile without dropping it
+// 	vert1 := tBokiVec.Vec2{X: float64(j * gridSize), Y: float64(i * gridSize)}
+// 	vert2 := tBokiVec.Vec2{X: float64((j+1)*gridSize - 1), Y: float64(i * gridSize)}
+// 	vert3 := tBokiVec.Vec2{X: float64((j+1)*gridSize - 1), Y: float64((i+1)*gridSize - 1)}
+// 	vert4 := tBokiVec.Vec2{X: float64(j * gridSize), Y: float64((i+1)*gridSize - 1)}
+
+// 	// Create a slice of vertices for the smaller rectangle
+// 	vertices := []tBokiVec.Vec2{vert1, vert2, vert3, vert4}
+
+// 	// Add rigid body to the scene using scenesUtil.AddBlockEntityPoly
+// 	scenesUtil.AddBlockEntityPoly(sys.scene, vertices)
+
+// 	// Mark the individual block as processed (not the entire rectangle)
+// 	markAsProcessed(collisionMatrix, i, j, 1, 1)
+// }
